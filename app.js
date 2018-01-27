6382dd8bed7cfa4b6f9e138692846716b64b60b1
Anson Miu <ansonmiu0214@gmail.com>
2018-01-27 21:23:22 +0000

    Handled webcam capture to be propagated to back-end for API usage

--- app.js
+++ app.js
@@ -1,13 +1,30 @@
 const express = require('express')
 const path = require('path')
 const http = require('http')
+const fs = require('fs')
+
 const app = express()
+const bodyParser = require('body-parser')
+app.use(bodyParser.json({ limit: '16mb' }))
+app.use(bodyParser.urlencoded({ extended: false, limit: '16mb'}))
+
+app.use(express.static('captures'))
 
 const PORT = process.env.PORT || 5050
 
 app.get('/', (req, res) => {
   console.log(__dirname)
   res.sendFile(__dirname + '/index.html')
+})
+
+app.post('/upload', (req, res) => {
+  let data = req.body.img.replace(/^data:image\/png;base64,/, "")
+  data += data.replace('+', ' ')
+  let binaryData = new Buffer(data, 'base64').toString('binary')
+  let name = 'captures/' + req.body.name
+  
+  fs.writeFile(name, binaryData, 'binary', (err) => console.log(err))
+  // console.log(`Received ${dataUrl}`)
 })
 
 app.listen(PORT, () => console.log(`Listening on port ${PORT}`))
