0733ac3dfed7805235b212b829aed487f580ffa2
Anson Miu <ansonmiu0214@gmail.com>
2018-01-27 22:19:01 +0000

    Added dependency for request module to try make ajax request within call to /upload API

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require('express')
 const path = require('path')
 const http = require('http')
 const fs = require('fs')
+const request = require('request')
 
 const app = express()
 const bodyParser = require('body-parser')
@@ -21,11 +22,27 @@ app.post('/upload', (req, res) => {
   let data = req.body.img.replace(/^data:image\/png;base64,/, "")
   data += data.replace('+', ' ')
   let binaryData = new Buffer(data, 'base64').toString('binary')
-  let name = 'public/captures/photo.png'
-  // let name = 'public/captures/' + req.body.name
+  // let name = 'public/captures/photo.png'
+  let name = 'public/captures/' + req.body.name
   
   fs.writeFile(name, binaryData, 'binary', (err) => console.log(err))
   // console.log(`Received ${dataUrl}`)
+
+  let requestName = 'https://damp-anchorage-78125.herokuapp.com' + '/captures/' + req.body.name
+
+  request({
+    url: "https://westus.api.cognitive.microsoft.com/emotion/v1.0/recognize",
+    method: "POST",
+    headers: {
+      'Content-Type': 'application/json',
+      'Ocp-Apim-Subscription-Key': 'daecb17d1425499199cefea44c3a38c4'
+    },
+    body: {
+      url: "https://damp-anchorage-78125.herokuapp.com/captures/"
+    }  
+  }, (err, res, body) => {
+    console.log(body)
+  })
 })
 
 app.listen(PORT, () => console.log(`Listening on port ${PORT}`))
