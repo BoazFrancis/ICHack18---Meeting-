d65a5d4f3b69eabd84f79d032eec94e64442ccdd
Anson Miu <ansonmiu0214@gmail.com>
2018-01-28 05:58:32 +0000

    Debugging error 503

--- app.js
+++ app.js
@@ -42,8 +42,8 @@ app.use(express.static('public'))
 
 // Default route
 app.get('/', (req, res) => {
-  pitchData = []
-  timeStamp = 0
+  // pitchData = []
+  // timeStamp = 0
   res.sendFile(__dirname + '/index.html')
 })
 
