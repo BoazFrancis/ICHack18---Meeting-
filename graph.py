98981c87110550f18b82d6a99ae4a5a84a2b7e60
Anson Miu <ansonmiu0214@gmail.com>
2018-01-28 05:35:43 +0000

    Deleted unused resources

--- graph.py
+++
@@ -1,9 +0,0 @@
-#!/bin/python3
-
-import sys, json
-
-if __name__ == "__main__":
-  data_obj = json.loads(sys.argv[1])
-
-  times = len(data_obj)
-  print("Python received data for {} timestamps".format(times))
