0b4a1a39c2936981ef579b5d721d5b5a0ad33b51
Anson Miu <ansonmiu0214@gmail.com>
2018-01-28 00:11:46 +0000

    Debugging Python implementation

--- graph.py
+++ graph.py
@@ -1,5 +1,5 @@
 #!/bin/python3
 
 if __name__ == "__main__":
-  msg = input().strip()
-  print("Python received input {}".format(msg))
+  print("Hello world")
+  # print("Python received input {}".format(msg))
